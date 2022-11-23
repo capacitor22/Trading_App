8e762f020825846712575e1bf7166f08ee5ab03d
capacitor22 <riccapaz@hotmail.com>
2022-11-23 06:17:28 -0300

    Removendo arquivos não necessários

--- config.py
+++
@@ -1,6 +0,0 @@
-# EndPoint
-# https://paper-api.alpaca.markets
-
-BASE_URL = "https://paper-api.alpaca.markets"
-API_KEY = "PK9MRDNW4ZX99EDUXXRU"
-SECRET_KEY = "bQsnZhDZWTlKev1QRpSgMOJ749f6BhBJr02uF9wR"
